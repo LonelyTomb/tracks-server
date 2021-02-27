c44c28f3d35688d74971dd1c131edb9ce0191460
VIctory <garubav@gmail.com>
2021-02-27 14:34:51 +0100

    use env PORT

--- src/index.js
+++ src/index.js
@@ -35,6 +35,6 @@ mongoose.connection.on("error", (err) => {
   console.error("error connecting to mongo instance", err);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000");
 });
